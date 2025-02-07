7ae4138de15f46144e780f1b6f0e4fa8ebea2c91
jisoo <doh_0112@naver.com>
2025-02-07 11:27:35 +0900

    chore: 사업자 등록증 사진 추가

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,20 @@ export default function Home() {
   return (
     <div className="mx-auto flex max-w-[700px] flex-col items-center justify-center">
       <p className="heading-1">Wisdomhub</p>
+      <div className="my-5 flex gap-5 heading-4">
+        <Link href={"/privacy-policy"}>개인정보 처리방침</Link>
+        <Link href={"/service-terms"}>서비스 이용약관</Link>
+      </div>
       <div className="flex gap-4">
         <Image src={"/1.png"} width={200} height={544} alt="위즈덤허브" />
         <Image src={"/2.png"} width={200} height={544} alt="위즈덤허브" />
       </div>
-      <div className="mt-10 flex gap-5 heading-4">
-        <Link href={"/privacy-policy"}>개인정보 처리방침</Link>
-        <Link href={"/service-terms"}>서비스 이용약관</Link>
-      </div>
+      <Image
+        src={"/simji.png"}
+        alt="심지 사업자 등록증"
+        width={400}
+        height={700}
+      />
     </div>
   );
 }
